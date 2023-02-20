56e55063eb5168244588623ececa31a5d95ffc6e
Ntheo <napsatarov@gmail.com>
2023-02-20 20:23:03 +0600

    simple greeting with name inp

--- src/main/java/kg/edu/alatoo/tasktracker/controllers/GreetingController.java
+++ src/main/java/kg/edu/alatoo/tasktracker/controllers/GreetingController.java
@@ -3,15 +3,22 @@ package kg.edu.alatoo.tasktracker.controllers;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PostMapping;
 
 @Controller
 public class GreetingController {
 
 	@GetMapping("/greeting")
-	public String greeting(@RequestParam(name="name", required=false, defaultValue="World") String name, Model model) {
-		model.addAttribute("name", "Nazar");
-		return "greeting";
+	public String greetingForm(Model model) {
+	  model.addAttribute("greeting", new Greeting());
+	  return "greeting";
+	}
+
+	@PostMapping("/greeting")
+	public String greetingSubmit(@ModelAttribute Greeting greeting, Model model) {
+	  model.addAttribute("greeting", greeting);
+	  return "result";
 	}
 
 }
