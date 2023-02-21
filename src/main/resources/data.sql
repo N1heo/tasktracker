1a20ece57da522fc84d0d29c8a10128b359765c0
Ntheo <napsatarov@gmail.com>
2023-02-21 11:18:46 +0600

    small changes

--- src/main/resources/data.sql
+++ src/main/resources/data.sql
@@ -1,2 +1,2 @@
-INSERT INTO task (description, issuer, executor, status) VALUES ('Coffee', 'Mark', 'Bill', 0);
-INSERT INTO task (description, issuer, executor, status) VALUES ('Mobile app', 'Elon', 'Mark', 1);
+-- INSERT INTO task (description, issuer, executor, status) VALUES ('Coffee', 'Mark', 'Bill', 0);
+-- INSERT INTO task (description, issuer, executor, status) VALUES ('Mobile app', 'Elon', 'Mark', 1);
